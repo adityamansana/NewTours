46b1ea6f81ca80a25e0f03b73761501cad10b255
Aditya <Aditya@DESKTOP-A8FPK08>
2018-03-28 23:04:37 +0530

    Committing the orange hrm leave assignment, partially incomplete.

--- src/ObjectRepositiory/MercuryToursPageObjects.java
+++ src/ObjectRepositiory/MercuryToursPageObjects.java
@@ -21,8 +21,24 @@ public class MercuryToursPageObjects {
 	public static By cmbBox_InDate  = By.cssSelector("select[name='toDay']");
 	public static By cmbBox_Airline  = By.cssSelector("select[name='airline']");
 	public static By btn_Continue = By.cssSelector("input[name='findFlights']");
-	
+	public static By txtFromDate = By.id("assignleave_txtFromDate");
 	public static By caption_Text = By.xpath("//form[@name=\"results\"]/descendant::b[contains(text(),'Price')][4]");
+	public static By btnCalanderNext = By.xpath("//span[@class='ui-icon ui-icon-circle-triangle-e']");
+	public static By txtToDate = By.id("assignleave_txtToDate");
+	public static By btnSelectFromDate = By.xpath("//a[contains(text(),'11')]");
+	public static By btnSelectToDate = By.xpath("//a[contains(text(),'15')]");
+	public static By btnAssignLeave = By.id("assignBtn");
+	public static By txtSearchEmployeeByName = By.id("assignleave_txtEmployee_empName");
+	public static By btnLeaveOnMainMenu = By.id("menu_leave_viewLeaveModule");
+	public static By btnLeaveSubMenu = By.id("menu_leave_assignLeave");
+	public static By btnLeaveReports = By.id("menu_leave_Reports");
+	public static By btnLeaveBalanceReport = By.id("menu_leave_viewLeaveBalanceReport");
+	public static By cmbBox_SelectLeaveSearchType = By.id("leave_balance_report_type");
+	
+	public static By txtLeave_EmployeeName = By.id("leave_balance_employee_empName");
+	public static By txtEmployeeNametoSearchLeave = By.xpath("//strong[contains(text(),'Han')]");
+	public static By btnAssignLeaveConfirmation = By.id("confirmOkButton");
+	public static By btnViewLeave = By.id("viewBtn");
 	
 	//form[@name="results"]/descendant::b[contains(text(),'Price')][4]/parent::*/parent::*/parent::*/parent::*/preceeding-sibling::*
 	//form[@name="results"]/descendant::b[contains(text(),'Price')][4]/parent::*/parent::*/parent::*/parent::*/preceding-sibling::tr/child::*/input
--- src/OrangeHRM/OrangeHRM_Main.java
+++ src/OrangeHRM/OrangeHRM_Main.java
@@ -5,18 +5,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import ObjectRepositiory.MercuryToursPageObjects;
+
 public class OrangeHRM_Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		OrangeHRM or = new OrangeHRM();
 		SelAction se = new SelAction();
+		MercuryToursPageObjects po = new MercuryToursPageObjects();
 	//	String o = "CHECK";
 	//	String p ="UNCHECK";
 		String UserRole = "ESS";
-		String UserName = "robert";
+		String UserName = "fg0004";
 		String LeaveType = "Vacation US";
 		By LeaveTypeLocatorId = By.id("assignleave_txtLeaveType");
+		String SearchLeaveType = "Employee";
 		
 		By ComboBoxObjInfo = By.name("searchSystemUser[userType]");
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Training\\chromedriver.exe");
@@ -30,27 +34,44 @@ public class OrangeHRM_Main {
 		Actions action = new Actions(driver);
 		action.moveToElement(driver.findElement(By.id("menu_admin_viewAdminModule"))).moveToElement(driver.findElement(By.id("menu_admin_UserManagement"))).moveToElement(driver.findElement(By.id("menu_admin_viewSystemUsers"))).click().build().perform();
 		
-		if(se.SelectComboBoxValue(driver, ComboBoxObjInfo, UserRole))
+   /*		if(se.SelectComboBoxValue(driver, ComboBoxObjInfo, UserRole))
 		{
-			/*se.SelectAllCheckBox(driver, o);
-			System.out.println("Checked the check box");
-			se.SelectAllCheckBox(driver, p);
-			System.out.println("Unchecked the check box");
-			 */
+		//	se.SelectAllCheckBox(driver, o);
+		//	System.out.println("Checked the check box");
+		//	se.SelectAllCheckBox(driver, p);
+		//	System.out.println("Unchecked the check box");
+			 
 			//se.SelectUserCheckBox(driver, UserName);
 			//System.out.println("Selected the check box for user: " +UserName);
 			se.SelectUserCheckBox_XpathAxes(driver, UserName);
 			
-		}
+		}   */
 		
-		action.moveToElement(driver.findElement(By.id("menu_leave_viewLeaveModule"))).moveToElement(driver.findElement(By.id("menu_leave_assignLeave"))).click().build().perform();
-		driver.findElement(By.id("assignleave_txtEmployee_empName")).sendKeys("han");
+		action.moveToElement(driver.findElement(po.btnLeaveOnMainMenu)).moveToElement(driver.findElement(po.btnLeaveSubMenu)).click().build().perform();
+		driver.findElement(po.txtSearchEmployeeByName).sendKeys("han");
 		action.moveToElement(driver.findElement(By.xpath("/html/body/div[6]/ul/li/strong"))).click().build().perform();
 		if(se.SelectComboBoxValue(driver, LeaveTypeLocatorId, LeaveType))
 		{
 			
-			action.moveToElement(driver.findElement(By.id("assignleave_txtFromDate"))).click().build().perform();
-			System.out.println(se.getFutureDate());
+			action.moveToElement(driver.findElement(po.txtFromDate)).click().build().perform();
+			action.moveToElement(driver.findElement(po.btnCalanderNext)).click().build().perform();
+			action.moveToElement(driver.findElement(po.btnSelectFromDate)).click().build().perform();
+			//Thread.sleep(5000);
+			action.moveToElement(driver.findElement(po.txtToDate)).click().build().perform();
+			action.moveToElement(driver.findElement(po.btnCalanderNext)).click().build().perform();
+			action.moveToElement(driver.findElement(po.btnSelectToDate)).click().build().perform();
+			Thread.sleep(2000);
+			driver.findElement(po.btnAssignLeave).click();
+			Thread.sleep(2000);
+			driver.findElement(po.btnAssignLeaveConfirmation).click();
+			action.moveToElement(driver.findElement(po.btnLeaveReports)).click().build().perform();
+			action.moveToElement(driver.findElement(po.btnLeaveBalanceReport)).click().build().perform();
+			if(se.SelectComboBoxValue(driver, po.cmbBox_SelectLeaveSearchType, SearchLeaveType))
+			{	
+			driver.findElement(po.txtLeave_EmployeeName).sendKeys("han");
+			action.moveToElement(driver.findElement(po.txtEmployeeNametoSearchLeave)).click().build().perform();
+			driver.findElement(po.btnViewLeave).click();
+			}
 		}
 		}
 	}
